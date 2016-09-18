6d68e08e06b920d611679597d5d1b28cb74a9150
ww0010 <73912910@qq.com>
2016-09-18 20:43:15 +0800

    new 08

--- src/com/atguigu/shiro/realm/New01.java
+++ src/com/atguigu/shiro/realm/New01.java
@@ -3,7 +3,8 @@ package com.atguigu.shiro.realm;
 public class New01 {
   private int a =1;
   private int b =1;
-  private Stirng = "asdasdas";
+  private String s = "asdasdas";
+  private String d = "sssss";
   public static void main(String[] args) {
 	
 	  
